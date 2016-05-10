37d29866de735edb46b9718bb2b582fb46f523d7
MasumMushfiq <masummushfiq94@gmail.com>
2016-05-10 07:24:26 +0600

    Updating Example.java

--- Java/Example.java
+++ Java/Example.java
@@ -1,6 +1,6 @@
 public class Example {
 	public static void main(String args[]) {
-        System.out.println("Hello World.");
+        System.out.println("Hello Git.");
         System.out.println("This is Mushfiq. :)");
     }
 }
